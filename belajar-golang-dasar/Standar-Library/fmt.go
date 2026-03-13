8022df8e4b61a09a5e7c0b7a7943040e25fa9c2d
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-13 19:26:42 +0700

    standar-Library

---
+++ belajar-golang-dasar/Standar-Library/fmt.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	firstname := "Haikal"
+	lastname := "Frastiawan"
+
+	fmt.Println("hello '", firstname,lastname,"'")
+	fmt.Printf("hello '%s %s' \n", firstname,lastname)
+	
+}
