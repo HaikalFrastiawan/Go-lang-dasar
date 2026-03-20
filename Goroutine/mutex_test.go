9282f8a7be93c155771ed5862ad6380e19cb6959
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-20 17:12:49 +0700

    Goroutine - Mutex (Handle Race Condition)

--- Goroutine/mutex_test.go
+++ Goroutine/mutex_test.go
@@ -2,18 +2,22 @@ package goroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestRaceCondition(t *testing.T) {
+func TestMutex(t *testing.T) {
 	x := 0
+	var mutex sync.Mutex
+
 	//total 100.000
 	for i := 1; i <= 1000; i++ {
 		go func() {
 			for j := 1; j <= 100; j++ {
-				
+				mutex.Lock()
 				x += 1
+				mutex.Unlock()
 			}
 		}()
 	}
