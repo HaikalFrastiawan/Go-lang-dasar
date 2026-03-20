8c01dc93e6b907ff3049b706476daeb374cdbfee
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-20 13:57:02 +0700

    Goroutine - Only in and Out Channel

--- Goroutine/channel_test.go
+++ Goroutine/channel_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
 )
 
 func TestCreateChannel(t *testing.T){
@@ -21,6 +22,45 @@ func TestCreateChannel(t *testing.T){
 	//menerima data
 	data := <- channel
 	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+//channel as parameter
+func  GiveMeResponse(channel chan string){
+	time.Sleep(2 * time.Second)
+	channel <- "Haikal Frastiawan"
+}
+
+func TestChannelAsParameter(t *testing.T){
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+//channel in and out
+func OnlyIn (channel chan<- string){
+	time.Sleep(2 * time.Second)
+	channel <- "Haikal Frastiawan"
+}
+
+func OnlyOut (channel <-chan string){
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInAndOutChannel(t *testing.T){
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
 
 	time.Sleep(5 * time.Second)
 }
