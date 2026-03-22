29abe12a179d375a2d27ae2c1afa14c011171aa4
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-22 08:22:33 +0700

    Context - With Value

--- Context/context_test.go
+++ Context/context_test.go
@@ -13,3 +13,31 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	//Add Context
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextB, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextC, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	//Get Value Context
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+
+	//ini karena parent
+	fmt.Println(contextA.Value("b"))
+}
