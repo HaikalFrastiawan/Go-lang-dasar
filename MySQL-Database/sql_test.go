8b67b3b499c35a0d91b119d679e69377c17324fe
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-23 14:15:35 +0700

    Database - TipeDataColoum

--- MySQL-Database/sql_test.go
+++ MySQL-Database/sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -42,7 +43,45 @@ func TestQuerySql(t *testing.T) {
 			panic(err)
 		}
 		fmt.Println("Id: ", id)
-		fmt.Println("Name:", name)
+		fmt.Println("Name:	", name)
 	}
+
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating , birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx,script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+    var id, name, email string
+    var balance int32
+    var rating float64
+    var birthDate, createdAt time.Time
+    var married bool
+
+    err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+    if err != nil {
+        panic(err)
+    }
+		fmt.Println("================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		fmt.Println("Email:", email)
+		fmt.Println("Balance:", balance)
+		fmt.Println("Rating:", rating)
+		fmt.Println("Birth Date:", birthDate)
+		fmt.Println("Married:", married)
+		fmt.Println("Created At:", createdAt)
+}
+defer rows.Close()
 
 }
