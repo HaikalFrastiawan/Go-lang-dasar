b9f069291e299ae10c9e332b235ce74778a1fe77
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-23 15:25:17 +0700

    Database - SQL Injection

--- MySQL-Database/sql_test.go
+++ MySQL-Database/sql_test.go
@@ -85,3 +85,84 @@ func TestQuerySqlComplex(t *testing.T) {
 defer rows.Close()
 
 }
+
+func TestSqlInjection(t *testing.T) {
+
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "admin"
+
+
+	script := "SELECT username FROM user WHERE username = '"+username+"' AND password = '"+password+"' LIMIT 1"
+
+	rows, err := db.QueryContext(ctx,script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses Login", username)
+	}else {
+		fmt.Println("Gagal Login")
+	}
+}
+
+
+func TestSqlInjectionSave(t *testing.T) {
+
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "admin"
+
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(script)
+
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses Login", username)
+	}else {
+		fmt.Println("Gagal Login")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	username := "eko"
+	password := "eko"
+
+	script := "INSERT INTO user(username, password) VALUES(?,?)"
+	_, err := db.ExecContext(ctx,script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success Insert New User")
+}
