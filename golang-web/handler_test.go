b5060059f8cd431243ab5880f2af6c27185b449e
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-25 06:41:38 +0700

    Web - Serve Mux

--- golang-web/handler_test.go
+++ golang-web/handler_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+
 )
 
 func TestHandler(t *testing.T) {
@@ -23,5 +24,39 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 
+}
 
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Hello World")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "images")
+	})
+
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "images thumbnails")
+	})
+
+		
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+
+
+
+	
 }
