e48e8c0dd2497445615ce17f0cd91b4102a21a3d
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-03-24 14:56:16 +0700

    Embed - File to byte

--- Embed/embed_test.go
+++ Embed/embed_test.go
@@ -3,14 +3,29 @@ package embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
 //go:embed version.txt
 var version string
 
+
 func TestString(t *testing.T) {
 	fmt.Println(version)
 	
-
 }
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	
+}
+
+
