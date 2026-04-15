d9227ec1b53be87f0134cb07713b90b66195851c
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-04-16 06:48:43 +0700

    Json - Decode

---
+++ golang-json/decode_json_test.go
@@ -0,0 +1,26 @@
+package golangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T) {
+	jsonSting := `{"FirstName":"Haikal","MiddleName":"Frastiawan","LastName":"Putra","Age":30,"Married":true}`
+
+	jsonBytes := []byte(jsonSting)
+
+	customer := &Customer{}
+
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.MiddleName)
+	fmt.Println(customer.LastName)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+}
