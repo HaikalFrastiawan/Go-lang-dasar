57e22b21d12d892823874196b908cb96f2d62abe
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-05-01 14:26:15 +0700

    DI - Injector

---
+++ dependency-injection/simple/injector.go
@@ -0,0 +1,14 @@
+//go:build wireinject
+// +build wireinject
+
+package simple
+
+
+func InitializeService() *SimpleService {
+	wire.Build(
+		NewSimpleRepository,NewSimpleService,
+	)
+	return nil
+}
+
+
