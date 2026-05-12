a6656840c0fe3abeb4e1952f4c4a1689e0352b62
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-05-13 06:39:47 +0700

    Generics - Make Project

---
+++ Generics/go.mod
@@ -0,0 +1,5 @@
+module golang-generics
+
+go 1.24.3
+
+require github.com/stretchr/testify v1.11.1 // indirect
---
+++ Logging/singleton_test.go
@@ -0,0 +1,16 @@
+package golang
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestSingleton(t *testing.T) {
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+}
