870c043acbc5f12c55ffc4eb60ece406866cdcbe
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-06-22 06:42:41 +0700

    Migration - Dirty State

--- Database-Migration/RestfulApi/app/database.go
+++ Database-Migration/RestfulApi/app/database.go
@@ -27,8 +27,14 @@ func NewDB() *sql.DB {
 
 //Migrasi ke Versi Tertentu (belakangnya tambahkan angka sesuai dengan versi yang diinginkan)
 //migrate -database "mysql://root@tcp(localhost:3306)/golang-database-migration" -path RestfulApi/db/migration up 1
-	
+
 //bikin migrate
 //migrate create -ext sql -dir RestfulApi/db/migration create_table_first
 //migrate create -ext sql -dir RestfulApi/db/migration create_table_second
 //migrate create -ext sql -dir RestfulApi/db/migration create_table_third
+
+//mengetahui versi migrasi
+//migrate -database "mysql://root@tcp(localhost:3306)/golang-database-migration" -path RestfulApi/db/migration version
+
+//dirty state menggubah versi (force)
+//migrate -database "mysql://root@tcp(localhost:3306)/golang-database-migration" -path RestfulApi/db/migration force <version>
