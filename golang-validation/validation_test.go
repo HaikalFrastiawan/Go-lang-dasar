45919c993404e9294bcc94a63278c2ec18e20869
haikalfrastiawan <haikalfrastiawan16@gmail.com>
2026-06-25 18:42:13 +0700

    Validation - Validasi Variabel

--- golang-validation/validation_test.go
+++ golang-validation/validation_test.go
@@ -1,6 +1,7 @@
 package validation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
@@ -10,5 +11,28 @@ func TestValidation(t *testing.T) {
 	var validate *validator.Validate = validator.New()
 	if validate == nil {
 		t.Error("Validate is nil")
+	}
+}
+
+func TestValidationVariabel(t *testing.T) {
+	validate := validator.New()
+
+	user := "eko"
+
+	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestValidateTwoVariabel(t *testing.T) {
+	validate := validator.New()
+
+	password := "rahasia"
+	confirmpassword := "rahasia"
+
+	err := validate.VarWithValue(password, confirmpassword, "eqfield")
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
